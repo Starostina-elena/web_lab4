b7fedaa4c861ac44f71b9d38612367a9e05ba32d
Starostina Helen <starostina.lena05@gmail.com>
2025-01-07 14:29:27 +0400

    login register page styles part2

--- frontend/src/views/LoginRegister.vue
+++ frontend/src/views/LoginRegister.vue
@@ -2,15 +2,17 @@
   <div class="about-me-container">
     <h1 id="about-me">Старостина Елена Петровна, P3219, ИСУ 414257. Вариант 19999</h1>
   </div>
-  <div id="login-form-container">
-    <LoginForm serverUrl="http://localhost:34350/lab4/api/auth/login"
-               defaultErrMessage="Неверный логин или пароль"
-               btnTitle="Войти" class="login-register-form"/>
-  </div>
-  <div id="register-form-container">
-    <LoginForm serverUrl="http://localhost:34350/lab4/api/auth/register"
-               defaultErrMessage="Пользователь с таким именем уже есть"
-               btnTitle="Зарегистрироваться" class="login-register-form"/>
+  <div class="form-wrapper">
+    <div id="login-form-container">
+      <LoginForm serverUrl="http://localhost:34350/lab4/api/auth/login"
+                 defaultErrMessage="Неверный логин или пароль"
+                 btnTitle="Войти" class="login-register-form"/>
+    </div>
+    <div id="register-form-container">
+      <LoginForm serverUrl="http://localhost:34350/lab4/api/auth/register"
+                 defaultErrMessage="Пользователь с таким именем уже есть"
+                 btnTitle="Зарегистрироваться" class="login-register-form"/>
+    </div>
   </div>
 </template>
 
@@ -40,11 +42,21 @@ h1 {
   }
 }
 
-@media (min-width: 784px) and (max-width: 1102px) {
-
+@media (min-width: 784px) {
+  #login-form-container, #register-form-container {
+    width: 48%;
+  }
+  .form-wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 20px;
+  }
 }
-
 @media (min-width: 1103px) {
-
+  #login-form-container {
+    margin-left: 15%;
+    margin-right: -15%;
+  }
 }
 </style>
